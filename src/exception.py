a986341f699866020114726128ee3946d78266cd
keerthikwarrier <keerthi.k.warrier@gmail.com>
2026-03-12 12:14:18 +0530

    Data Ingestion file created and executed

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,7 @@
 #This file is used for Exception handling
 
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
