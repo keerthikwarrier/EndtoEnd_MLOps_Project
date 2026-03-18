c0f3259ec0261c18a1f8e4857180c6579e870181
keerthikwarrier <keerthi.k.warrier@gmail.com>
2026-03-18 17:40:54 +0530

    Data Tranformation Done!

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,13 +1,16 @@
 #Collecting/Reading the Data from somewhere else for our project
-import os
-import sys
-from src.exception import CustomException
-from src.logger import logging
+import os #Used here to create folders and build file paths.
+import sys  #Here it is used for error handling to capture exception details.
+from src.exception import CustomException #Returns the exception occured
+from src.logger import logging #Logging records the events happening in the program.
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
-from dataclasses import dataclass #you can define the class variable
+from dataclasses import dataclass #dataclass automatically creates init methods and variables for classes.
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
+#Stores paths and settings
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts','train.csv')
@@ -46,6 +49,9 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,21 @@
 """Any functionality that need to be written in common for the entire application
 example if i want to read the dataset from a database i can write the MongoDB client here, if i want to save the model into the cloud"""
+
+import os
+import sys
+import dill
+
+import numpy as np
+import pandas as pd
+
+from src.exception import CustomException
+
+def save_object(file_path,obj):
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,"wb") as file_obj:
+            dill.dump(obj,file_obj)
+
+    except Exception as e:
+        raise CustomException(e,sys)
